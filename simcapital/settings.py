83dd71422ec14d7a868302f2896dd236c0018104
Gilson Paulino <gilsonbp@gmail.com>
2019-10-26 18:08:00 -0300

    Allowed hosts in settings

--- simcapital/settings.py
+++ simcapital/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.setdefault('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 if not DEBUG:
     ADMINS = (('Gilson Paulino', 'gilsonbp@gmail.com'),)
