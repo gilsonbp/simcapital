649ab8a2198fb30151ff181c2a6f581075334747
Gilson Paulino <gilsonbp@gmail.com>
2019-10-27 20:01:29 -0300

    Database connection in default settings

--- simcapital/settings.py
+++ simcapital/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'simcapital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.setdefault('POSTGRES_NAME', ''),
+        'USER': os.environ.setdefault('POSTGRES_USER', ''),
+        'PASSWORD': os.environ.setdefault('POSTGRES_PASSWORD', ''),
+        'HOST': os.environ.setdefault('POSTGRES_HOST', ''),
+        'PORT': os.environ.setdefault('POSTGRES_PORT', ''),
     }
 }
 
