328c86817b84d089f733aba2ccccb63cc22e25a9
Gilson Paulino <gilsonbp@gmail.com>
2019-10-27 14:22:17 -0300

    Testing create salary

--- apps/capital/test.py
+++ apps/capital/test.py
@@ -1,8 +1,9 @@
 from datetime import date
+from decimal import Decimal
 
 from django.test import TestCase
 
-from apps.capital.models import CapitalUser
+from apps.capital.models import CapitalUser, Salary
 
 
 class CapitalUserTestCase(TestCase):
@@ -21,3 +22,28 @@ class CapitalUserTestCase(TestCase):
         self.assertEqual(user.name, 'Teste User')
         self.assertEqual(user.cpf, '12345678901')
         self.assertEqual(user.birth_date, date(year=1983, month=10, day=31))
+
+
+class SalaryTestCase(TestCase):
+    def setUp(self):
+        user = CapitalUser.objects.create(
+            email='teste@teste.com',
+            name='Teste User',
+            cpf='12345678901',
+            birth_date='1983-10-31'
+        )
+        Salary.objects.create(
+            capital_user=user,
+            salary_date=date.today(),
+            salary_value=Decimal('2500.12'),
+            salary_discount=Decimal('100.12'),
+        )
+
+    def test_create_salary(self):
+        """ Testing if you can create a capital user """
+        salary = Salary.objects.get(
+            capital_user__email='teste@teste.com',
+            salary_date=date.today()
+        )
+        self.assertEqual(salary.salary_value, Decimal('2500.12'))
+        self.assertEqual(salary.salary_discount, Decimal('100.12'))
