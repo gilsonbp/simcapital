5b6f694b935f35463ab8c1f31bc0bc2f9d9d6762
Gilson Paulino <gilsonbp@gmail.com>
2019-10-26 21:20:28 -0300

    overriding the create method

--- apps/simauth/serializers.py
+++ apps/simauth/serializers.py
@@ -4,6 +4,27 @@ from apps.simauth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True,
+                                     style={'input_type': 'password'})
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = (
+            'id', 'email', 'name', 'is_superuser', 'password'
+        )
+
+    def create(self, validated_data):
+        if validated_data.get('is_superuser', False):
+            user = User.objects.create_superuser(
+                validated_data['email'],
+                validated_data['name'],
+                validated_data['password'],
+            )
+        else:
+            user = User.objects.create_user(
+                validated_data['email'],
+                validated_data['name'],
+                validated_data['password'],
+            )
+
+        return user
